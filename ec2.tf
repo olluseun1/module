99367f08ddb0b6630df0786580879c77824e5e44
olu <ollu1.ajani@gmail.com>
2023-07-08 14:09:15 +0100

    supply key var

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "a_machine" {
   ami           = "ami-00b1c9efd33fda707"
-  key_name      = "newkey"
+  key_name      = var.machine_key
   instance_type = "t2.micro"
 
   tags = {
@@ -10,5 +10,10 @@ resource "aws_instance" "a_machine" {
 
 variable machine_name {
     default = "Name me please"
+}
+
+variable "machine_key" {
+    default = "newkey"
+  
 }
   
